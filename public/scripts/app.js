c2d1708c74cf934e73dd462b4003e803765a8dfc
George Volokitin <saintgeo23@gmail.com>
2017-01-24 10:06:01 +0300

    MDP-09. WIP

---
+++ public/scripts/app.js
@@ -0,0 +1,17 @@
+(function () {
+'use strict';
+
+const app = new Vue({
+  el: '#content',
+  data: {
+    input: 'Heading\n=======\n\nSub-heading\n-----------\n\n### Another deeper heading\n\nParagraphs are separated\nby a blank line.\n\nLeave 2 spaces at the end of a line to do a  \nline break\n\nText attributes *italic*, **bold**,\n`monospace`,~~strikethrough~~ .\n\nShopping list:\n* apples\n* oranges\n* pears\n\nNumbered list:\n1. apples\n2. oranges\n3. pears\n\n*[George Volokitin](https://www.freecodecamp.com/saintgeo23)*'
+  },
+
+  computed: {
+    getResult() {
+      return marked(this.input);
+    }
+  }
+});
+
+}());
--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,12 +1,14 @@
-var app = new Vue({
+'use strict';
+
+const app = new Vue({
   el: '#content',
   data: {
     input: 'Heading\n=======\n\nSub-heading\n-----------\n\n### Another deeper heading\n\nParagraphs are separated\nby a blank line.\n\nLeave 2 spaces at the end of a line to do a  \nline break\n\nText attributes *italic*, **bold**,\n`monospace`,~~strikethrough~~ .\n\nShopping list:\n* apples\n* oranges\n* pears\n\nNumbered list:\n1. apples\n2. oranges\n3. pears\n\n*[George Volokitin](https://www.freecodecamp.com/saintgeo23)*',
   },
 
   computed: {
-    getResult: function () {
-      return html = marked(this.input);
+    getResult() {
+      return marked(this.input);
     }
   },
 });
